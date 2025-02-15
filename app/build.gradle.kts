609403dec65b78654d8f4b1838cbdfe80dd0b0b1
BRADDTELES <braddteles@gmail.com>
2025-02-14 23:28:15 -0300

    adicionando varias bibliotecas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
    alias(libs.plugins.android.application)
+   id("androidx.navigation.safeargs") //SafeArgs
    id("com.google.dagger.hilt.android")
 }
 
@@ -60,6 +61,29 @@ dependencies {
    implementation("androidx.palette:palette-ktx:1.0.0")
    implementation("androidx.room:room-ktx:2.6.1")
    implementation("androidx.sqlite:sqlite-ktx:2.4.0")
+
+   //Salvar dados em um banco de dados local usando o Room
+   val room_version = "2.6.1"
+   implementation("androidx.room:room-runtime:$room_version")
+   annotationProcessor("androidx.room:room-compiler:$room_version")
+
+   //Retrofit e Gson
+   implementation("com.squareup.retrofit2:retrofit:2.11.0")
+   implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+   //Picasso
+   implementation("com.squareup.picasso:picasso:2.8")
+   //EasyValidation
+   val ev_version = "1.0.4"
+   implementation("com.wajahatkarim:easyvalidation-core:$ev_version")
+   implementation("com.wajahatkarim:easyvalidation-toast:$ev_version")
+   //SplashScreen
+   implementation("androidx.core:core-splashscreen:1.0.0")
+   //Navigation
+   val nav_version = "2.8.7"
+   implementation("androidx.navigation:navigation-fragment:$nav_version")
+   implementation("androidx.navigation:navigation-ui:$nav_version")
+   //PermissionX
+   implementation("com.guolindev.permissionx:permissionx:1.8.1")
 
    implementation(libs.appcompat)
    implementation(libs.material)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
    alias(libs.plugins.android.application) apply false
    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
+//SafeArgs
+buildscript {
+   repositories {
+      google()
+      mavenCentral()//PermissionX
+   }
+   dependencies {
+      val nav_version = "2.8.6"
+      classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+   }
+}
